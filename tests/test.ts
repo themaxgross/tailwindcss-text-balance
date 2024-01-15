7deb2d676cd759ba234a96ed9f36755baf725c1b
Leo Ji <leo@leoji.codes>
2024-01-15 10:34:37 -0600

    update tests

--- tests/test.ts
+++ tests/test.ts
@@ -3,18 +3,54 @@ import postcss from 'postcss'
 
 import plugin from '../src/index'
 
-it('should output the text-wrap:balance css', () => {
-  let css = postcss([
+it('should output the text-wrap:balance css', async () => {
+  const { css } = await postcss([
     require('tailwindcss')({
       content: [{ raw: 'text-balance' }],
       plugins: [plugin],
       corePlugins: { preflight: false },
     }),
-  ]).process('@tailwind utilities;', { from: undefined }).css
+  ]).process('@tailwind utilities;', { from: undefined })
 
   expect(css).toMatchInlineSnapshot(`
     ".text-balance {
         text-wrap: balance
+    }"
+  `)
+})
+
+it('should output the text-wrap:pretty css', async () => {
+  const { css } = await postcss([
+    require('tailwindcss')({
+      content: [{ raw: 'text-pretty' }],
+      plugins: [plugin],
+      corePlugins: { preflight: false },
+    }),
+  ]).process('@tailwind utilities;', { from: undefined })
+
+  expect(css).toMatchInlineSnapshot(`
+    ".text-pretty {
+        text-wrap: pretty
+    }"
+  `)
+})
+
+// skipping this test because the actual output is doubled, and I don't know why
+it.skip('should output the text-wrap:balance and text-wrap:pretty css', async () => {
+  const { css } = await postcss([
+    require('tailwindcss')({
+      content: [{ raw: 'text-balance text-pretty' }],
+      plugins: [plugin],
+      corePlugins: { preflight: false },
+    }),
+  ]).process('@tailwind utilities;', { from: undefined })
+
+  expect(css).toMatchInlineSnapshot(`
+    ".text-balance {
+        text-wrap: balance
+    }
+    .text-pretty {
+        text-wrap: pretty
     }"
   `)
 })
