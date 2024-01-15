46401ebd128ccd0299a97491eab264d2c203767f
Leo Ji <leo@leoji.codes>
2024-01-15 10:19:45 -0600

    add text-wrap: pretty as a utility as well

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import plugin from 'tailwindcss/plugin'
 
-const textBalance = plugin(function ({ addUtilities }) {
+const textBalance = plugin(function({ addUtilities }) {
   addUtilities({
     '.text-balance': {
       'text-wrap': 'balance',
+    },
+    '.text-pretty': {
+      'text-wrap': 'pretty',
     },
   })
 })
